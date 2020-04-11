d91340007586d61521b89a8ffc1246a81c6ec650
Luis Pablo <diganluispa@gmail.com>
2020-04-10 20:11:04 -0600

    Adding results to all exercises

--- bob/bob.go
+++ bob/bob.go
@@ -53,37 +53,51 @@ func Hey(message string) string {
 }
 
 func cleanString(input string) string {
+	// Only left the letters, numbers and some symbols
 	reg, err := regexp.Compile("[^a-zA-Z0-9?!,:)]+")
+
+	// If some error ocurs, panic
 	if err != nil {
 		panic(err)
 	}
+
+	// Replace all the chars non setted in the regex by empty string
 	return reg.ReplaceAllString(input, "")
 }
 
+// Validate if the input have only uppercase letters
 func isUpperCase(input string) bool {
+	// Validate only all the letters
 	reg, err := regexp.Compile("[^a-zA-Z]+")
 	if err != nil {
 		panic(err)
 	}
 
+	// Replace all the non letters by empty string
 	inputReplaced := reg.ReplaceAllString(input, "")
 
+	// If the result string is empty, is not uppercase
 	if len(inputReplaced) == 0 {
 		return false
 	}
 
 	isUpper := true
 
+	// Iterate in the result strig searching for lowercases
 	for _, r := range inputReplaced {
+		// If some value is a letter and is not a Uppercase
 		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
 			isUpper = false
 			break
 		}
 	}
 
+	// Return the result of the iteration
 	return isUpper
 }
 
+// isQuestion returns if the string is a question
 func isQuestion(input string) bool {
+	// Search for the last char, and validate if is equal to `?`
 	return input[len(input)-1:] == "?"
 }
